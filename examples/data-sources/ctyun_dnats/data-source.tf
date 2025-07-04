980dddd786eaa9cf1a78ae1c55112db47187c3e6
lity9 <lity9@chinatelecom.cn>
2025-07-04 13:52:43 +0800

    fix doc

--- examples/data-sources/ctyun_dnats/data-source.tf
+++
@@ -1,3 +0,0 @@
-data "ctyun_nat_dnats" "test" {
-  nat_gateway_id = "natgw-asdsmh8scy"
-}
--- examples/data-sources/ctyun_nat_snats/data-source.tf
+++ examples/data-sources/ctyun_nat_snats/data-source.tf
@@ -6,6 +6,9 @@ terraform {
   }
 }
 
+provider "ctyun" {
+  env = "prod"
+}
 
 data "ctyun_nat_snats" "test"{
   nat_gateway_id = "natgw-asdsmh8scy"
