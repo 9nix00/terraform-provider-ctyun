6173effd1881bc46f5dd75c9f8e32be0b63e3065
jiajsh <jiajsh@chinatelecom.cn>
2025-08-14 11:25:09 +0800

    文档与校验fix

--- internal/service/ecs/resource_ctyun_ecs_backup_policy.go
+++ internal/service/ecs/resource_ctyun_ecs_backup_policy.go
@@ -66,7 +66,7 @@ type CtyunEcsBackupPolicyAdvRetention struct {
 
 func (c *ctyunEcsBackupPolicy) Schema(_ context.Context, _ resource.SchemaRequest, response *resource.SchemaResponse) {
 	response.Schema = schema.Schema{
-		MarkdownDescription: `**详细说明请见文档：https://www.ctyun.cn/document/10026730/10141018**`,
+		MarkdownDescription: `**详细说明请见文档：https://www.ctyun.cn/document/10026751/10033770**`,
 		Attributes: map[string]schema.Attribute{
 			"id": schema.StringAttribute{
 				Computed:    true,
