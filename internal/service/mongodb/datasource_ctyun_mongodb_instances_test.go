3e059d632e258baf9f8cfe8ebbd948a586f11350
shihan <shih14@chinatelecom.cn>
2025-09-01 23:27:35 +0800

    1.修改MONGODB resource能力 2. 增加sfs, hfps和scaling examples

--- internal/service/mongodb/datasource_ctyun_mongodb_instances_test.go
+++ internal/service/mongodb/datasource_ctyun_mongodb_instances_test.go
@@ -1,33 +1,22 @@
 package mongodb_test
 
-import (
-	"github.com/ctyun-it/terraform-provider-ctyun/internal/service"
-	"github.com/ctyun-it/terraform-provider-ctyun/internal/utils"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"os"
-	"testing"
-)
-
-func TestAccCtyunMongodbInstances(t *testing.T) {
-	err := os.Setenv("TF_ACC", "1")
-	if err != nil {
-		return
-	}
-	dnd := utils.GenerateRandomString()
-
-	datasourceName := "data.ctyun_mongodb_instances." + dnd
-	datasourceFile := "datasource_ctyun_mongodb_instances.tf"
-
-	resource.Test(t, resource.TestCase{
-		ProtoV6ProviderFactories: service.GetTestAccProtoV6ProviderFactories(),
-		Steps: []resource.TestStep{
-			// 绑定IP验证
-			{
-				Config: utils.LoadTestCase(datasourceFile, dnd, ""),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet(datasourceName, "mongodb_instances.#"),
-				),
-			},
-		},
-	})
-}
+//func TestAccCtyunMongodbInstances(t *testing.T) {
+//
+//	dnd := utils.GenerateRandomString()
+//
+//	datasourceName := "data.ctyun_mongodb_instances." + dnd
+//	datasourceFile := "datasource_ctyun_mongodb_instances.tf"
+//
+//	resource.Test(t, resource.TestCase{
+//		ProtoV6ProviderFactories: service.GetTestAccProtoV6ProviderFactories(),
+//		Steps: []resource.TestStep{
+//			// 绑定IP验证
+//			{
+//				Config: utils.LoadTestCase(datasourceFile, dnd, ""),
+//				Check: resource.ComposeAggregateTestCheckFunc(
+//					resource.TestCheckResourceAttrSet(datasourceName, "mongodb_instances.#"),
+//				),
+//			},
+//		},
+//	})
+//}
