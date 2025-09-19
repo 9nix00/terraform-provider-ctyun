943ffc1af084a1f433314b309ba40f2ecd801f14
LITY9_CHINATELECOM.CN <lity9@chinatelecom.cn>
2025-09-19 09:11:31 +0800

    fix ecs and ebm password

--- internal/extend/terraform/validator/validator_ecs_password.go
+++ internal/extend/terraform/validator/validator_ecs_password.go
@@ -16,7 +16,7 @@ func EcsPassword() validator.String {
 }
 
 func (v validatorEcsPassword) Description(_ context.Context) string {
-	return "不满足ecs密码要求"
+	return "不满足密码要求"
 }
 
 func (v validatorEcsPassword) MarkdownDescription(ctx context.Context) string {
@@ -30,15 +30,30 @@ func (v validatorEcsPassword) ValidateString(_ context.Context, request validato
 	password := request.ConfigValue.ValueString()
 	length := len(password)
 	if length < 8 || length > 30 {
-		errMessage := "ecs密码长度必须在8-30"
+		errMessage := "密码长度必须在8-30"
 		response.Diagnostics.AddError(errMessage, errMessage)
 		return
 	}
 	if strings.HasPrefix(password, "/") {
-		errMessage := "ecs密码不能以/开头"
+		errMessage := "密码不能以/开头"
 		response.Diagnostics.AddError(errMessage, errMessage)
 		return
 	}
+
+	for i := 0; i <= len(password)-3; i++ {
+		// 获取当前字符和后两个字符的ASCII值
+		c1 := int(password[i])
+		c2 := int(password[i+1])
+		c3 := int(password[i+2])
+
+		// 检查是否连续递增（如a(97)、b(98)、c(99)）
+		if c2 == c1+1 && c3 == c2+1 {
+			errMessage := "密码不能包含3个及以上连续字符，如abc，123"
+			response.Diagnostics.AddError(errMessage, errMessage)
+			return
+		}
+	}
+
 	hasUpperLetter := false
 	hasLowerLetter := false
 	hasDigit := false
@@ -53,7 +68,7 @@ func (v validatorEcsPassword) ValidateString(_ context.Context, request validato
 		} else if isHasSpecialSymbols(r) {
 			hasSpecialSymbols = true
 		} else {
-			errMessage := "ecs密码只能为数字，大小写字母以及特殊符号"
+			errMessage := "密码只能为数字，大小写字母以及特殊符号"
 			response.Diagnostics.AddError(errMessage, errMessage)
 			return
 		}
@@ -74,7 +89,7 @@ func (v validatorEcsPassword) ValidateString(_ context.Context, request validato
 	}
 
 	if count < 3 {
-		errMessage := "ecs密码必须包含大小写字母、数字、特殊符号中的至少三种"
+		errMessage := "密码必须包含大小写字母、数字、特殊符号中的至少三种"
 		response.Diagnostics.AddError(errMessage, errMessage)
 		return
 	}
