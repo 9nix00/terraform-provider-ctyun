14660c901b83658bfa8fe1b8baf224ef3bc7a6ea
张志华 <zhangzhh13@chinatelecom.cn>
2025-10-24 09:26:00 +0800

    feat: pgsql 安全组

--- internal/core/ctyun-sdk-endpoint/pgsql/apis.go
+++ internal/core/ctyun-sdk-endpoint/pgsql/apis.go
@@ -3,24 +3,28 @@ package pgsql
 import ctyunsdk "github.com/ctyun-it/terraform-provider-ctyun/internal/core/ctyun-sdk-core"
 
 type Apis struct {
-	PgsqlDestroyApi             *TeledbDestroyApi
-	PgsqlCreateApi              *PgsqlCreateApi
-	PgsqlDetailApi              *PgsqlDetailApi
-	PgsqlListApi                *PgsqlListApi
-	PgsqlRefundApi              *PgsqlRefundApi
-	PgsqlRestartApi             *PgsqlRestartApi
-	PgsqlSecurityGroupListApi   *PgsqlSecurityGroupListApi
-	PgsqlSpecsApi               *PgsqlSpecsApi
-	PgsqlStartApi               *PgsqlStartApi
-	PgsqlStopApi                *PgsqlStopApi
-	PgsqlUpdateInstanceNameApi  *PgsqlUpdateInstanceNameApi
-	PgsqlUpdateSecurityGroupApi *PgsqlUpdateSecurityGroupApi
-	PgsqlUpgradeApi             *PgsqlUpgradeApi
-	PgsqlBindEipApi             *PgsqlBindEipApi
-	PgsqlUnBindEipApi           *PgsqlUnBindEipApi
-	PgsqlBoundEipListApi        *PgsqlBoundEipListApi
-	PgsqlDeleteSecurityGroupApi *PgsqlDeleteSecurityGroupApi
-	PgsqlGetNodeListApi         *PgsqlGetNodeListApi
+	PgsqlDestroyApi                  *TeledbDestroyApi
+	PgsqlCreateApi                   *PgsqlCreateApi
+	PgsqlDetailApi                   *PgsqlDetailApi
+	PgsqlListApi                     *PgsqlListApi
+	PgsqlRefundApi                   *PgsqlRefundApi
+	PgsqlRestartApi                  *PgsqlRestartApi
+	PgsqlSecurityGroupListApi        *PgsqlSecurityGroupListApi
+	PgsqlSpecsApi                    *PgsqlSpecsApi
+	PgsqlStartApi                    *PgsqlStartApi
+	PgsqlStopApi                     *PgsqlStopApi
+	PgsqlUpdateInstanceNameApi       *PgsqlUpdateInstanceNameApi
+	PgsqlUpdateSecurityGroupApi      *PgsqlUpdateSecurityGroupApi
+	PgsqlUpgradeApi                  *PgsqlUpgradeApi
+	PgsqlBindEipApi                  *PgsqlBindEipApi
+	PgsqlUnBindEipApi                *PgsqlUnBindEipApi
+	PgsqlBoundEipListApi             *PgsqlBoundEipListApi
+	PgsqlDeleteSecurityGroupApi      *PgsqlDeleteSecurityGroupApi
+	PgsqlGetNodeListApi              *PgsqlGetNodeListApi
+	PostgresqlCollectorPolicyApi     *PostgresqlCollectorPolicyApi
+	PostgresqlGetCollectorPolicyApi  *PostgresqlGetCollectorPolicyApi
+	PostgresqlAddSecurityGroupApi    *PostgresqlAddSecurityGroupApi
+	PostgresqlChangeSecurityGroupApi *PostgresqlChangeSecurityGroupApi
 }
 
 func NewApis(client *ctyunsdk.CtyunClient) *Apis {
@@ -33,24 +37,28 @@ func NewApis(client *ctyunsdk.CtyunClient) *Apis {
 	client.RegisterEndpoint(ctyunsdk.EnvironmentDev, EndpointPgSqlTest)
 	client.RegisterEndpoint(ctyunsdk.EnvironmentProd, EndPointPgSqlProd)
 	return &Apis{
-		PgsqlDestroyApi:             NewTeledbDestroyApi(client),
-		PgsqlCreateApi:              NewPgsqlCreateApi(client),
-		PgsqlDetailApi:              NewPgsqlDetailApi(client),
-		PgsqlListApi:                NewPgsqlListApi(client),
-		PgsqlRefundApi:              NewPgsqlRefundApi(client),
-		PgsqlRestartApi:             NewPgsqlRestartApi(client),
-		PgsqlSecurityGroupListApi:   NewPgsqlSecurityGroupListApi(client),
-		PgsqlSpecsApi:               NewPgsqlSpecsApi(client),
-		PgsqlStartApi:               NewPgsqlStartApi(client),
-		PgsqlStopApi:                NewPgsqlStopApi(client),
-		PgsqlUpdateInstanceNameApi:  NewPgsqlUpdateInstanceNameApi(client),
-		PgsqlUpdateSecurityGroupApi: NewPgsqlUpdateSecurityGroupApi(client),
-		PgsqlUpgradeApi:             NewPgsqlUpgradeApi(client),
-		PgsqlBindEipApi:             NewPgsqlBindEipApi(client),
-		PgsqlUnBindEipApi:           NewPgsqlUnBindEipApi(client),
-		PgsqlBoundEipListApi:        NewPgsqlBoundEipListApi(client),
-		PgsqlDeleteSecurityGroupApi: NewPgsqlDeleteSecurityGroupApi(client),
-		PgsqlGetNodeListApi:         NewPgsqlGetNodeListApi(client),
+		PgsqlDestroyApi:                  NewTeledbDestroyApi(client),
+		PgsqlCreateApi:                   NewPgsqlCreateApi(client),
+		PgsqlDetailApi:                   NewPgsqlDetailApi(client),
+		PgsqlListApi:                     NewPgsqlListApi(client),
+		PgsqlRefundApi:                   NewPgsqlRefundApi(client),
+		PgsqlRestartApi:                  NewPgsqlRestartApi(client),
+		PgsqlSecurityGroupListApi:        NewPgsqlSecurityGroupListApi(client),
+		PgsqlSpecsApi:                    NewPgsqlSpecsApi(client),
+		PgsqlStartApi:                    NewPgsqlStartApi(client),
+		PgsqlStopApi:                     NewPgsqlStopApi(client),
+		PgsqlUpdateInstanceNameApi:       NewPgsqlUpdateInstanceNameApi(client),
+		PgsqlUpdateSecurityGroupApi:      NewPgsqlUpdateSecurityGroupApi(client),
+		PgsqlUpgradeApi:                  NewPgsqlUpgradeApi(client),
+		PgsqlBindEipApi:                  NewPgsqlBindEipApi(client),
+		PgsqlUnBindEipApi:                NewPgsqlUnBindEipApi(client),
+		PgsqlBoundEipListApi:             NewPgsqlBoundEipListApi(client),
+		PgsqlDeleteSecurityGroupApi:      NewPgsqlDeleteSecurityGroupApi(client),
+		PgsqlGetNodeListApi:              NewPgsqlGetNodeListApi(client),
+		PostgresqlCollectorPolicyApi:     NewPostgresqlCollectorPolicyApi(client),
+		PostgresqlGetCollectorPolicyApi:  NewPostgresqlGetCollectorPolicyApi(client),
+		PostgresqlAddSecurityGroupApi:    NewPostgresqlAddSecurityGroupApi(client),
+		PostgresqlChangeSecurityGroupApi: NewPostgresqlChangeSecurityGroupApi(client),
 	}
 
 }
